59af917aa163d31c0a5734b8542ee2aca28e19d5
Hausemaster8281 <66825701+Hausemaster8281@users.noreply.github.com>
2025-03-01 09:55:27 +0530

    Incremented more properties

--- ListView_Alpha.dart
+++ ListView_Alpha.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatefulWidget{
+
+class MyApp extends StatefulWidget {
   @override
-  createState() => CounterState();
+  CounterState createState() => CounterState();
 }
-class CounterState extends State{
-  List items=['burger,'sandwitch','idly','dosa','uttapam','butter roll'];
-  List Qty=[0,0,0,0,0,0];
+
+class CounterState extends State<MyApp> {
+  List<String> items = ['burger', 'sandwich', 'idly', 'dosa', 'uttapam', 'butter roll'];
+  List<int> Qty = [0, 0, 0, 0, 0, 0];
+
   @override
-  Widget build(BuildContext context{
-    return Scaffold(
-    Drawer:Drawer(),
-    appBar:AppBar(
-  title:Text('MyKitched')),
-  body:ListView.builder(
-  itemCount: items.length,
-  itemBuilder:(
-  BuildContext context
-  ){
-    return Container(child:ListTitle(
-    title:Text('${
-  items[index]
-  }'),
-  trailing:Column(Children:[Text('Qty: ${
-  Qty[index]
-  }')])
-    ))
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(title: Text('MyKitchen')),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: ListTile(
+                title: Text('${items[index]}'),
+                trailing: Column(
+                  children: [
+                    Text('Qty: ${Qty[index]}'),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
-  )
-    )
-  })
 }
