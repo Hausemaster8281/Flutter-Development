e80e3f1e98ecfaaa7e9709d85041077e9eaffa72
Your Name <66825701+Hausemaster8281@users.noreply.github.com>
2025-03-27 10:08:48 +0530

    Feature addition
    Exception handling and function to give response when button is prressed

--- Form_Application.dart
+++ Form_Application.dart
@@ -3,57 +3,81 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatefulWidget
-{
+
+class MyApp extends StatefulWidget {
   @override
-  createState()=>MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class MyAppState extends State
-{
+class MyAppState extends State<MyApp> {
+  final _formKey = GlobalKey<FormState>();
+  String message = '';
+
+  void myValidator() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        message = 'Login successful';
+      });
+    } else {
+      setState(() {
+        message = 'Kindly fill all details';
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar:AppBar(
-          title:Text(
-            'Login Form'
-          )
+      home: Scaffold(
+        appBar: AppBar(title: Text('Login Form')),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    hintText: 'Enter your full name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: myValidator,
+                  child: Text('Login'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  message,
+                  style: TextStyle(color: Colors.red, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
         ),
-        body:Form(
-          child:ListView(
-            children:[
-              Container(
-                  margin:EdgeInsets.all(25.0),
-                child:TextFormField(
-                  decoration:
-                    InputDecoration(
-                      labelText:'Username',
-                      hintText:'Enter your full name'
-                    )
-                )
-              ),
-              Container(
-                  margin:EdgeInsets.all(25.0),
-                  child:TextFormField(
-                      decoration:
-                      InputDecoration(
-                          labelText:'Passowrd',
-                          hintText:'Enter your passowrd'
-                      )
-                  )
-              ),
-          Container(
-              margin:EdgeInsets.all(25.0),
-              child: ElevatedButton(
-                child:Text('Login'),
-                onPressed:() {}
-              )
-              )
-            ]
-          )
-        )
-      )
+      ),
     );
   }
 }
