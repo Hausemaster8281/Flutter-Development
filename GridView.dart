0a0ef722be92addab1364c099d7881cb17fd7fdd
Hausemaster8281 <66825701+Hausemaster8281@users.noreply.github.com>
2025-04-10 08:47:32 +0530

    Added GridView Program
    Three buttons implemented, who change their accent when pressed(anonymous arrow function used)

--- GridView.dart
+++ GridView.dart
@@ -3,20 +3,83 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-        drawer: Drawer(),
-        appBar: AppBar(title: Text('MyKitchen')),
-        body: GridView.Count(
-          crossAxisCount:3,
-          children: <Widget>[
-            InkWell(
-              onTapp:
-              child:card(
-            )
-            )
-    ]
-    )
+      title: 'Grid_View_Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // List to track the color state of each button
+  List<Color> buttonColors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+  ];
+
+  // Function to change color when a button is tapped
+  void changeColor(int index) {
+    setState(() {
+      // Cycle through some colors when tapped
+      if (buttonColors[index] == Colors.red) {
+        buttonColors[index] = Colors.redAccent;
+      } else if (buttonColors[index] == Colors.redAccent) {
+        buttonColors[index] = Colors.red;
+      } else if (buttonColors[index] == Colors.green) {
+        buttonColors[index] = Colors.greenAccent;
+      } else if (buttonColors[index] == Colors.greenAccent) {
+        buttonColors[index] = Colors.green;
+      } else if (buttonColors[index] == Colors.blue) {
+        buttonColors[index] = Colors.blueAccent;
+      } else if (buttonColors[index] == Colors.blueAccent) {
+        buttonColors[index] = Colors.blue;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('MyKitchen'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        padding: EdgeInsets.all(20.0),
+        children: List.generate(3, (index) {
+          return InkWell(
+            onTap: () => changeColor(index),
+            child: Card(
+              color: buttonColors[index],
+              elevation: 5.0,
+              child: Center(
+                child: Text(
+                  'Button ${index + 1}',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
